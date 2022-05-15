f2ec2fbe34fbeacca6e9763440b11757af39e259
will <willpspeight@gmail.com>
2022-05-15 10:36:08 -0400

    step 5: game timer is done

--- index.js
+++ index.js
@@ -144,6 +144,47 @@ function rechtangularCollision({rectangle1, rectangle2}) {
     )
 }
 
+//this determinds the winner
+function determineWinnder({player, enemy, timerId}){
+    //this will stop the time if one player wins before the time runs out 
+    clearTimeout(timerId)
+    //this will change the display style back to flex right now its on none 
+    document.querySelector('#displayText').style.display = 'flex'
+    if(player.health == enemy.health) {
+        // this will call the displayText Id and display tie when the timer runss out and both enemy and player have equal health
+        document.querySelector('#displayText').innerHTML = 'Tie'
+    }else if (player.health > enemy.health){
+        // this will call the displayText Id and display tie when the timer runss out and player has more health
+        document.querySelector('#displayText').innerHTML = 'Player 1 wins'
+    }else if(enemy.health > player.health){
+        // this will call the displayText Id and display tie when the timer runss out and enemy has more health
+        document.querySelector('#displayText').innerHTML = 'Player 2 wins'
+    }
+}
+
+// weare createing an infite loop in which setTimeout(decreaseTimer) is looping through each other same as animate loop
+let timer = 60
+let TimerId
+function decreaeTimer(){
+    if (timer > 0) {
+        timerId = setTimeout(decreaeTimer, 1000)
+        timer--
+        //this selectes the timer label we have index.html and the innerHTML = timer will take that 10 we have in the div and substract from it 
+        document.querySelector('#timer').innerHTML = timer
+    }
+
+    //this determines the winner when timer reaches 0
+    if(timer == 0){
+        //determineWinner Function call
+        determineWinnder({player, enemy, timerId})
+
+
+    }
+    
+}
+
+decreaeTimer()
+
 
 //animation loop 
 function animate(){
@@ -199,7 +240,12 @@ function animate(){
         player.health -= 20
         document.querySelector('#playerHealth').style.width = player.health + '%' 
         console.log('enemy attack worked')
-}
+    }
+
+    //end game based on health
+    if(enemy.health <= 0 || player.health <= 0){
+        determineWinnder({player, enemy, timerId})
+    }
 
 
     //prints out if the animation loop is working 
